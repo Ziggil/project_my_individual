a42289df39faa77dd2682c091db1f36981d1e838
Ziggil <23zahar051982@gmail.com>
2025-11-13 20:50:01 +0300

    regist_v1

--- my_website_browser1/main/views.py
+++ my_website_browser1/main/views.py
@@ -2,16 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    context={
-        'title':'Home',
-        'content':'Сайт обучения WEB!',
-        'list':[22,26,29],
-        'user_auted':True
-    }
+   #можно добавить список или кортеж, словарь
 
-    return render(request, 'main/index.html', context)
+    return render(request, 'main/index.html')
 
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+
+def regist(request):
+    return render(request, 'main/regist.html')
+
+
+
--- my_website_browser1/my_website_browser/urls.py
+++ my_website_browser1/my_website_browser/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -23,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('regist/', views.regist, name='regist'),
 ]
