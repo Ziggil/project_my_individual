540fa1d129114d0b93429811eb1cf758f44ceb0b
Ziggil <23zahar051982@gmail.com>
2026-01-21 23:48:20 +0300

    +base шаблон, поредактировал css, возникла ошибка с подключением base в home

--- my_website_browser1/main/views.py
+++ my_website_browser1/main/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render
-
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -16,6 +15,9 @@ def about(request):
 @login_required
 def home(request):
     return render(request, 'main/home.html')
+
+def base(request):
+    return render(request, 'main/base.html')
 
 # и ко всем следующим страницам примеянй этот декоратор!
 
--- my_website_browser1/my_website_browser/urls.py
+++ my_website_browser1/my_website_browser/urls.py
@@ -9,8 +9,9 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('base/', views.base, name='base'),
     # path('register/', views.register, name='register'),
     path('users/', include('users.urls', namespace="users" )),
 
