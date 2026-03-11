fbadd72fabada1fd0dc216fcbd810b568fb1d4da
Ziggil <23zahar051982@gmail.com>
2026-03-11 19:54:02 +0300

    написал код в js, ПРИМЕЧАНИЕ: переменная назвается difficluty! ошибка в js из-за знака в modalBody.innerHTML

--- my_website_browser1/main/static/deps/js/main_quiz.js
+++ my_website_browser1/main/static/deps/js/main_quiz.js
@@ -1,1 +1,26 @@
 console.log("hello world")
+
+const modalBtns=[...document.getElementsByClassName('modal-button')]
+const modalBody=document.getElementById('modal-body-confirm')
+
+modalBtns.forEach(modalBtn=>modalBtn.addEventListener('click',()=>{
+    const pk=modalBtn.getAttribute('data-pk')
+    const name=modalBtn.getAttribute('data-quiz')
+    const numQuestions=modalBtn.getAttribute('data-questions')
+    const difficluty=modalBtn.getAttribute('data-difficluty')
+    const scoreToPass=modalBtn.getAttribute('data-pass')
+    const time=modalBtn.getAttribute('data-time')
+
+    modalBody.innerHTML=
+        `<div>Уверены ли вы что хотите начать?"<b>${name}</b>"?</div>
+         <div>
+            <ul>
+                <li>сложность:<b>${difficluty}</b></li>
+                <li>количество вопросов:<b>${numQuestions}</b></li>
+                <li>баллы для прохождения:<b>${scoreToPass}</b></li>
+                <li>время:<b>${time}минут</b></li>
+            <ul>
+         </div>
+    `
+        
+}));
